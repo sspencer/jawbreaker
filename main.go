441acb6514f81b37f326d7c7f5fcdf8b8cdfea77
Steve Spencer <steve@bigfrog.net>
2024-09-11 20:57:59 -0700

    track scores

--- main.go
+++ main.go
@@ -1,60 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"html/template"
-	"io"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
-	"sync"
+	"strconv"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
-	Rows        = 12
-	Cols        = 12
 	DefaultPort = "5454"
 )
 
 var (
-	Pieces      = []string{"🟣", "🔵", "🟢", "🔴", "🟡"}
-	globalScore GlobalScore
+	db *sql.DB
 )
 
-type GameResults struct {
-	Date       int      `json:"date"`
-	DailyBoard []string `json:"daily_board,omitempty"`
-	Score      int      `json:"score"`
-	Moves      int      `json:"moves"`
-	Pieces     int      `json:"pieces"`
-}
-
-type PageData struct {
-	Rows int `json:"rows"`
-	Cols int `json:"cols"`
-	GameResults
-}
-
-type GlobalScore struct {
-	GameResults
-	mu sync.RWMutex
+type GameStats struct {
+	GamesPlayedAllTime int `json:"games_played_all_time"`
+	GamesPlayedToday   int `json:"games_played_today"`
+	HighScore          int `json:"high_score"`
 }
 
 func main() {
-	//globalScore = GlobalScore{}
-	maybeResetGlobalScores()
+	initDB()
 
 	mount := os.Getenv("MOUNT")
-	http.HandleFunc("POST "+mount+"/scores", saveScoresHandler)
-	http.HandleFunc("GET "+mount+"/scores", retrieveScoresHandler)
 	http.HandleFunc("GET "+mount+"/", indexHandler)
-	http.HandleFunc("GET "+mount+"/index.html", indexHandler)
+	http.HandleFunc("POST "+mount+"/score/{score}", scoreHandler) // Submit a score
+	http.HandleFunc("GET "+mount+"/stats", statsHandler)          // Get today's highest score
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -65,6 +44,18 @@ func main() {
 	if err := http.ListenAndServe(":"+port, LoggingMiddleware(http.DefaultServeMux)); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Initialize the SQLite database
+func initDB() {
+	var err error
+	db, err = sql.Open("sqlite3", "file:scores.db?cache=shared")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	execPragmas()
+	createTables()
 }
 
 func LoggingMiddleware(next http.Handler) http.Handler {
@@ -78,48 +69,60 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	templatePath := filepath.Join("index.html")
-	tmpl, err := template.ParseFiles(templatePath)
-	if err != nil {
-		handleServerError(w, "Error parsing template", err)
-		return
-	}
-
-	data := getPageData()
-
-	if err := tmpl.Execute(w, data); err != nil {
-		handleServerError(w, "Error executing template", err)
-		return
-	}
+	http.ServeFile(w, r, "index.html")
 }
 
-func retrieveScoresHandler(w http.ResponseWriter, r *http.Request) {
-	maybeResetGlobalScores()
-	results := getGameResults()
-
-	err := sendJSON(w, http.StatusOK, results)
+func scoreHandler(w http.ResponseWriter, r *http.Request) {
+	value := r.PathValue("score")
+	score, err := strconv.Atoi(value)
 	if err != nil {
-		handleServerError(w, "Error sending response", err)
+		http.Error(w, "Invalid score", http.StatusBadRequest)
 		return
 	}
+
+	_, err = db.Exec("INSERT INTO game_scores (score) VALUES (?)", score)
+	if err != nil {
+		log.Printf("%s: %s\n", "scoreHandler", err.Error())
+		http.Error(w, "Failed to record score", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK")
 }
 
-func saveScoresHandler(w http.ResponseWriter, r *http.Request) {
-	var input GameResults
-	err := decodeJSON(w, r, &input, true)
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+	// Get UTC date in the format YYYY-MM-DD
+	utc := time.Now().UTC().Format("2006-01-02")
+	message := "statsHandler"
+
+	var gamesPlayedAllTime int
+	err := db.QueryRow("SELECT count(*) FROM game_scores").Scan(&gamesPlayedAllTime)
 	if err != nil {
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
+		log.Printf("%s: %s\n", message, err.Error())
 	}
 
-	results := updateGlobalScores(input)
-
-	log.Printf("score=%d, moves=%d, pieces=%d\n", results.Score, results.Moves, results.Pieces)
-
-	err = sendJSON(w, http.StatusOK, results)
+	var gamesPlayedToday int
+	err = db.QueryRow("SELECT count(*) FROM game_scores WHERE DATE(timestamp) = ?", utc).Scan(&gamesPlayedToday)
 	if err != nil {
-		handleServerError(w, "Error sending response", err)
-		return
+		log.Printf("%s: %s\n", message, err.Error())
+	}
+
+	var highScore int
+	err = db.QueryRow("SELECT MAX(score) FROM game_scores WHERE DATE(timestamp) = ?", utc).Scan(&highScore)
+	if err != nil {
+		log.Printf("%s: %s\n", message, err.Error())
+	}
+
+	stats := GameStats{
+		GamesPlayedAllTime: gamesPlayedAllTime,
+		GamesPlayedToday:   gamesPlayedToday,
+		HighScore:          highScore,
+	}
+
+	err = sendJSON(w, http.StatusOK, stats)
+	if err != nil {
+		handleServerError(w, "Failed to send JSON", err)
 	}
 }
 
@@ -138,145 +141,64 @@ func sendJSON(w http.ResponseWriter, status int, data any) error {
 	return err
 }
 
-func decodeJSON(w http.ResponseWriter, r *http.Request, dst interface{}, disallowUnknownFields bool) error {
-	maxBytes := 1024
-	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
-
-	dec := json.NewDecoder(r.Body)
-
-	if disallowUnknownFields {
-		dec.DisallowUnknownFields()
-	}
-
-	err := dec.Decode(dst)
-	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-		var invalidUnmarshalError *json.InvalidUnmarshalError
-
-		switch {
-		case errors.As(err, &syntaxError):
-			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
-
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			return errors.New("body contains badly-formed JSON")
-
-		case errors.As(err, &unmarshalTypeError):
-			if unmarshalTypeError.Field != "" {
-				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
-			}
-			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
-
-		case errors.Is(err, io.EOF):
-			return errors.New("body must not be empty")
-
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			return fmt.Errorf("body contains unknown key %s", fieldName)
-
-		case err.Error() == "http: request body too large":
-			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
-
-		case errors.As(err, &invalidUnmarshalError):
-			panic(err)
-
-		default:
-			return err
-		}
-	}
-
-	err = dec.Decode(&struct{}{})
-	if err != io.EOF {
-		return errors.New("body must only contain a single JSON value")
-	}
-
-	return nil
-}
-
 func handleServerError(w http.ResponseWriter, message string, err error) {
 	log.Printf("%s: %s\n", message, err.Error())
 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 }
 
-func generateDailyBoard(seed int) []string {
-	rnd := rand.New(rand.NewSource(int64(seed)))
-	size := Rows * Cols
-	num := len(Pieces)
-	board := make([]string, size)
-	for i := 0; i < size; i++ {
-		board[i] = Pieces[rnd.Intn(num)]
+func execPragmas() {
+	var err error
+	// Set various pragmas to optimize the database connection.
+	_, err = db.Exec("PRAGMA journal_mode=WAL;")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return board
-}
-
-func currentDate() int {
-	now := time.Now()
-	return now.Year()*10000 + int(now.Month())*100 + now.Day()
-}
-
-func getPageData() PageData {
-	data := PageData{
-		Rows: Rows,
-		Cols: Cols,
-	}
-	globalScore.mu.RLock()
-	data.GameResults = globalScore.GameResults
-	globalScore.mu.RUnlock()
-	return data
-}
-
-func getGameResults() GameResults {
-	data := GameResults{}
-	globalScore.mu.RLock()
-	data = globalScore.GameResults
-	data.DailyBoard = nil
-	globalScore.mu.RUnlock()
-	return data
-}
-
-func updateGlobalScores(input GameResults) GameResults {
-	now := currentDate()
-
-	globalScore.mu.Lock()
-	defer globalScore.mu.Unlock()
-
-	if globalScore.Date == now && input.Date == now {
-		if input.Score > globalScore.Score {
-			globalScore.Score = input.Score
-		}
-		if globalScore.Moves == 0 || input.Moves < globalScore.Moves {
-			globalScore.Moves = input.Moves
-		}
-		if globalScore.Pieces == 0 || input.Pieces < globalScore.Pieces {
-			globalScore.Pieces = input.Pieces
-		}
-	} else {
-		globalScore.Date = now
-		globalScore.DailyBoard = generateDailyBoard(now)
-		globalScore.Score = 0
-		globalScore.Moves = 0
-		globalScore.Pieces = 0
+	_, err = db.Exec("PRAGMA synchronous=NORMAL;")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	data := GameResults{}
-	data = globalScore.GameResults
-	return data
-}
-
-func maybeResetGlobalScores() {
-	date := currentDate()
-	globalScore.mu.Lock()
-	defer globalScore.mu.Unlock()
-
-	if globalScore.Date == date {
-		return // If the dates are the same, do nothing
+	_, err = db.Exec("PRAGMA foreign_keys=ON;")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Update the global score
-	globalScore.Date = date
-	globalScore.DailyBoard = generateDailyBoard(date)
-	globalScore.Score = 0
-	globalScore.Moves = 0
-	globalScore.Pieces = 0
+	_, err = db.Exec("PRAGMA cache_size=-64000;") // Set cache size.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("PRAGMA busy_timeout=30000;") // Set busy timeout.
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createTables() {
+	scoresTable := `
+	CREATE TABLE IF NOT EXISTS game_scores (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		score INTEGER,
+		timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+	);`
+
+	indexDate := `CREATE INDEX IF NOT EXISTS idx_game_date ON game_scores(timestamp);`
+	indexScoreDate := `CREATE INDEX IF NOT EXISTS idx_game_score_date ON game_scores(score, timestamp);`
+
+	var err error
+	_, err = db.Exec(scoresTable)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(indexDate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(indexScoreDate)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
